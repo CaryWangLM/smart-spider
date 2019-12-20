59d72e5944f246fbef00d6339299e143ecb0e53c
wanglimin <wanglimin@qunqun.io>
2019-12-20 11:34:33 +0800

    master

--- src/mn.js
+++ src/mn.js
@@ -1,30 +1,31 @@
 const puppeteer = require('puppeteer');
 const { mn } = require('./config/default');
 const srcToimg = require('./helper/srcToimg');
+const { argv } = process;
 
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://image.baidu.com/');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(argv[2]);
 
-    await page.setViewport({
-      width: 1920,
-      height: 1080
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  });
+
+  await page.focus('#kw');
+  await page.keyboard.sendCharacter(argv[3]);
+  await page.click('.s_search');
+
+  page.on('load', async () => {
+
+    const srcs = await page.evaluate(() => {
+      const images = document.querySelectorAll('img.main_img');
+      return Array.prototype.map.call(images, img => img.src);
+    })
+    srcs.forEach(async src => {
+      await srcToimg(src, mn);
     });
-
-    await page.focus('#kw');
-    await page.keyboard.sendCharacter('狗');
-    await page.click('.s_search');
-
-    page.on('load', async () => {
-
-      const srcs = await page.evaluate(() => {
-        const images = document.querySelectorAll('img.main_img');
-        return Array.prototype.map.call(images, img => img.src);
-      })
-      srcs.forEach(async src => {
-        await srcToimg(src, mn);
-      });
-      await browser.close();
-    });
-  })()
+    await browser.close();
+  });
+})()
--- src/screenshot.js
+++ src/screenshot.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
 const { screenshot } = require('./config/default');
+const { argv } = process;
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.baidu.com');
+  await page.goto(argv[2]);
   await page.screenshot({
     path: `${screenshot}/${Date.now()}.png`
   })
